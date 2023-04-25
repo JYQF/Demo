797ab2b54d925c3a86d0b0c2ca0d39433b1bab0c
JYQF <gwq17866836583@163.com>
2023-04-25 08:40:07 +0800

    MySQL做对象版

--- commodity_manage1.py
+++ commodity_manage1.py
@@ -19,9 +19,9 @@ class mysql:
         cursor.execute(query,(id,name,sum,price,data))
         self.conn.commit()
 
-    def update(self, id, name, sum, price,data):
+    def update(self, name, sum, price,data,id):
         cursor = self.conn.cursor()
-        query = "update  commodity update commodity set name=%s,sum=%s,price=%s,data=%s where id=%s"
+        query = "update commodity set name=%s,sum=%s,price=%s,data=%s where id=%s"
         cursor.execute(query,(name,sum,price,data,id))
         self.conn.commit()
 
@@ -38,9 +38,39 @@ class mysql:
         results = cursor.fetchall()
         return results
 
+    def show_single(self,id):
+        cursor = self.conn.cursor()
+        query = "select * from commodity where id =%s"
+        cursor.execute(query,(id))
+        result = cursor.fetchone()
+        return result
+
     def show_all(self):
         query = "select * from commodity"
         result = self.inquire(query)
+        return result
+
+    def checkC(self,id):
+        cursor = self.conn.cursor()
+        #返回指定条件下满足查询条件的数据行数（即统计数量），而不是返回所有匹配的行
+        query = "select count(*) from commodity where id = %s"
+        cursor.execute(query,(id))
+        result = cursor.fetchone()
+        return result
+
+    def checkSingle(self,id):
+        cursor = self.conn.cursor()
+        #返回所有匹配查询条件的行及其所有列的值
+        query = "select * from commodity where id = %s"
+        cursor.execute(query, (id))
+        result = cursor.fetchone()
+        return result
+
+    def checkT(self):
+        cursor = self.conn.cursor()
+        query = "select * from commodity"
+        cursor.execute(query)
+        result = cursor.fetchall()
         return result
 
     def quit(self):
@@ -55,7 +85,7 @@ class commodity:
         self.price = price
         self.data = data
 
-class manager():
+class manager:
     def __init__(self,mysql):
         self.mysql = mysql
 
@@ -65,14 +95,19 @@ class manager():
         sum = input("请输入商品数量:")
         price = input("请输入商品价格:")
         data = input("请输入商品备注:")
-
-        coms = self.show_all()
-
-        if any(c.id == commodity.id for c in coms):
-            print("此商品已经存在！")
-        else:
-            self.mysql.add(id, name, sum, price,data)
+        result = self.mysql.checkC(id)
+        if result[0] == 0:
+            self.mysql.add(id,name,sum,price,data)
             print("添加商品成功！")
+        else:
+            print("此商品已经存在！")
+        #coms = self.show_all()
+        #com = commodity(id,name,sum,price,data)
+        # if any(c.id == com.id for c in coms):
+        #     print("此商品已经存在！")
+        # else:
+        #     self.mysql.add(id, name, sum, price,data)
+        #     print("添加商品成功！")
 
     def update_com(self):
         id = input("请输入想要修改的商品编号:")
@@ -80,31 +115,59 @@ class manager():
         sum = input("请输入新的商品数量:")
         price = input("请输入新的商品价格:")
         data = input("请输入新的商品备注:")
-        coms = self.show_all()
 
-        if not any(c.id == commodity.id for c in coms):
-            print("此商品不存在！")
-        else:
-            self.mysql.update(id,name,sum,price,data)
+        if self.mysql.checkC(id)[0] > 0:
+            self.mysql.update(name,sum,price,data,id)
             print("修改商品成功！")
+        else:
+            print("此商品不存在！")
+        #com = commodity(id,name,sum,price,data)
+        #coms = self.show_all()
+
+        # if not any(c.id == id for c in coms):
+        #     print("此商品不存在！")
+        # else:
+        #     self.mysql.update(name,sum,price,data,id)
+        #     print("修改商品成功！")
 
     def delete_com(self):
         id = input("请输入要删除的商品编号:")
-        coms = self.show_all()
 
-        if not any(c.id == commodity.id for c in coms):
-            print("此商品不存在！")
-        else:
+        if self.mysql.checkC(id)[0] > 0:
             self.mysql.delete(id)
             print("删除商品成功！")
+        else:
+            print("此商品不存在！")
+        #coms = self.show_all()
+
+        # if not any(c.id == id for c in coms):
+        #     print("此商品不存在！")
+        # else:
+        #     self.mysql.delete(id)
+        #     print("删除商品成功！")
+    def show_single(self):
+        id = input("请输入要查找的商品编号:")
+        result = self.mysql.checkSingle(id)
+
+        if result is None:
+            print("查找的商品不存在！")
+        else:
+            print(f"商品编号:{result[0]} 商品名称:{result[1]} 商品数量:{result[2]} 商品价格:{result[3]} 商品信息:{result[4]}")
+
 
     def show_all(self):
-        results = self.mysql.show_all()
-        coms = [commodity(result[0],result[1],result[2],result[3],result[4]) for result in results]
-        return coms
+        result = self.mysql.checkT()
+        # coms = [commodity(result[0],result[1],result[2],result[3],result[4]) for result in results]
+        # return coms
+        if len(result) == 0:
+            print("商品信息为空！")
+        else:
+            print("-----------------商品信息-------------------")
+            for i in result:
+                print(f"商品编号:{i[0]} 商品名称:{i[1]} 商品数量:{i[2]} 商品价格:{i[3]} 商品信息:{i[4]}")
 
 #创建mysql对象
-mysql = mysql(host='localhost',user='host',password='wqwy041803160322',database='commodity')
+mysql = mysql(host='localhost',user='root',password='wqwy041803160322',database='commodity')
 #连接数据库
 mysql.connect()
 
@@ -118,7 +181,7 @@ while True:
     print("------4.查看单个商品-------")
     print("------5.查看所有商品-------")
     print("------6.退出系统----------")
-    choice = input("请输入要进行的操作：")
+    choice = input("请输入要进行的操作:")
 
     if choice == '1':
         manager.add_com()
@@ -127,19 +190,9 @@ while True:
     elif choice == '3':
         manager.delete_com()
     elif choice == '4':
-        id = input("请输入想要查看的商品编号:")
-        coms = manager.show_all()
-
-        if not any(c.id == id for c in coms):
-            print("此商品不存在！")
-        else:
-            #next() 获取迭代器的下一个元素
-            result = next(c for c in coms if c.id == id)
-            print(f"商品编号:{result.id} 商品名称:{result.name} 商品数量:{result.sum} 商品价格:{result.price} 商品信息:{result.data}")
+        manager.show_single()
     elif choice == '5':
-        coms = manager.show_all()
-        for i in coms:
-            print(f"商品编号:{i.id} 商品名称:{i.name} 商品数量:{i.sum} 商品价格:{i.price} 商品信息:{i.data}")
+        manager.show_all()
     elif choice == '6':
         break
     else:
